849a00cf63b38a5bfdb1971f7c88b6816102fab1
anna-aleksandrova <108029971+anna-aleksandrova@users.noreply.github.com>
2023-10-04 10:16:27 +0300

    Update task_04_10.py

--- task_04_10.py
+++ task_04_10.py
@@ -1,8 +1,11 @@
 n = int(input())
 product = 1
-for i in range(1, n+1):
-    if i % 8 == 0:
-        product *= i
-    else:
-        pass
-print(product)
+if n >= 8:
+    for i in range(1, n+1):
+        if i % 8 == 0:
+            product *= i
+        else:
+            pass
+    print(product)
+else:
+    print(0)
